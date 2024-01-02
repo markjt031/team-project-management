3856b01d4f8098303cf0e4328a0a833e54e94cca
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 <YuriSobioch@gmail.com>
2024-01-02 13:38:56 -0700

    navbar created

---
+++ frontend/src/app/company-select/company-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-select',
+  templateUrl: './company-select.component.html',
+  styleUrls: ['./company-select.component.css']
+})
+export class CompanySelectComponent {
+
+}
