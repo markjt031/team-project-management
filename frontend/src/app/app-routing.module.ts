03b808e46b6dcc239d6ce6a153cf4aa03796a020
markjt031 <markjt031@gmail.com>
2024-01-02 14:37:50 -0500

    added routing for teams and projects

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamsComponent } from './teams/teams.component';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'teams', component: TeamsComponent},
+  {path: 'teams/:id/projects', component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
