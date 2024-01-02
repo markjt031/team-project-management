f9e158bc6a901c587aadb932830edb592c80cbb9
markjt031 <markjt031@gmail.com>
2024-01-02 16:21:16 -0500

    added new components to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TeamsComponent } from './teams/teams.component';
 import { TeamCardComponent } from './teams/team-card/team-card.component';
 import { AddTeamButtonComponent } from './teams/add-team-button/add-team-button.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AddTeamModalComponent } from './teams/add-team-modal/add-team-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamsComponent,
     TeamCardComponent,
-    AddTeamButtonComponent
+    AddTeamButtonComponent,
+    ProjectsComponent,
+    AddTeamModalComponent
   ],
   imports: [
     BrowserModule,
