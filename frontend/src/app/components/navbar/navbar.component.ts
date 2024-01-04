f1d5318641b963c490ad8401ce6206814f5a4d27
PhamJimmy <JimmyPhamRes@gmail.com>
2024-01-03 22:12:35 -0800

    login and logout changes to localstorage

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
   open: boolean = false;
 
+  constructor(private userService: UserService) {}
+
   menuClick() {
-    this.open = !this.open
+    this.open = !this.open;
   }
+
+  logout = () => {
+    this.userService.logOutUser();
+  };
 }
--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { User, Credentials } from './models';
 import { fetchData } from './services/api';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +24,8 @@ export class UserService {
   });
   currentUser = this.user.asObservable();
 
+  constructor(private router: Router) {}
+
   updateUser = (user: User) => {
     this.user.next(user);
   };
@@ -39,8 +42,28 @@ export class UserService {
 
       const userData: User = await fetchData(`users/login`, options);
       this.updateUser(userData);
+      localStorage.setItem('user', JSON.stringify(userData));
     } catch (error) {
       console.error(error);
     }
+  };
+
+  logOutUser = () => {
+    this.updateUser({
+      id: -1,
+      profile: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      },
+      admin: false,
+      active: false,
+      status: '',
+      companies: [],
+      teams: [],
+    });
+    localStorage.removeItem('user');
+    this.router.navigate(['/login']);
   };
 }
