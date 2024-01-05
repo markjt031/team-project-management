c7b783ee3417cf5bc009f212d423ea90f8b9d11d
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 <YuriSobioch@gmail.com>
2024-01-04 20:44:51 -0700

    user registyr endpoints connected

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { UserService } from '../user.service';
+import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'app-users',
@@ -7,7 +9,9 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  constructor(private userData: UserService, private companyData: CompanyService) {}
 
+  companyId: number = 2;
   data = [
     {name: 'Chris Purnell', email: 'yocrizzle@gmail.com', team: 'Chic-Fila', active: true, admin: true, status: 'JOINED'},
     {name: 'Frank Fournier', email: 'foshizzle@gmail.com', team: 'PopEyes', active: true, admin: true, status: 'JOINED'},
@@ -28,10 +32,11 @@ export class UsersComponent {
 
   ngOnInit() {
     this.getData()
+    this.companyData.currentCompany.subscribe(company => this.companyId = company.id)
   }
 
   async getData() {
-    let res = await fetch('http://localhost:8080/company/2')
+    let res = await fetch('http://localhost:8080/company/' + this.companyId)
     let company = await res.json()
 
     let employeeArr = []
@@ -79,7 +84,63 @@ export class UsersComponent {
     this.makeUserAdmin = s;
   }
 
-  handleSubmit() {
-    console.log({...this.userForm.value, admin: this.makeUserAdmin === "YES" ? true : false})
+  async handleSubmit() {
+    const form = this.userForm.value
+
+    let currentUserCredentials = localStorage.getItem('credentials')
+    if(currentUserCredentials == null) currentUserCredentials = JSON.stringify({username: 'hi', password: 'hi'})
+
+    const userRequestDto = {
+      credentials: { 
+        username: form.firstName.charAt(0) + form.lastName,
+        password: form.password
+      },
+      profile: {
+        firstName: form.firstName,
+        lastName: form.lastName,
+        email: form.email,
+        phone: '0000000000'
+      },
+      admin: this.makeUserAdmin === "YES" ? true : false
+    }
+
+    const currentUserRequestDto = {
+      credentials: JSON.parse(currentUserCredentials),
+      profile: {},
+      admin: false
+    }
+
+    this.userData.currentUser.subscribe(user => {
+      currentUserRequestDto.profile = user.profile
+      currentUserRequestDto.admin = user.admin
+    })
+
+    let res = await fetch('http://localhost:8080/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userRequestDto)
+    }) 
+    
+    let jsonres = await res.json()
+    console.log(jsonres)
+
+    setTimeout( async () => {
+      res = await fetch('http://localhost:8080/company/' + this.companyId + '/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          admin: currentUserRequestDto,
+          newEmployeeId: jsonres.id
+        })
+      })
+      let success = await res.json()
+      console.log(success)
+      if(success.id) window.location.reload()
+    }, 2000)
+
   }
 }
