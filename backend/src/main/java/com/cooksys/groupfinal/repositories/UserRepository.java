725b96e48d790bac2f0da7011690b70a8dbd6521
markjt031 <markjt031@gmail.com>
2024-01-06 10:46:23 -0500

    added ability to find user by username or email.

--- backend/src/main/java/com/cooksys/groupfinal/repositories/UserRepository.java
+++ backend/src/main/java/com/cooksys/groupfinal/repositories/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findByCredentialsUsernameAndActiveTrue(String username);
-
+	Optional<User> findByCredentialsUsername(String username);
+	Optional<User> findByProfileEmail(String email);
 }
