34b4bb4846ce2659d1bac4f3c24a968b1890010d
markjt031 <markjt031@gmail.com>
2024-01-06 10:46:03 -0500

    added mapping for NewTeamRequestDto

--- backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
+++ backend/src/main/java/com/cooksys/groupfinal/mappers/TeamMapper.java
@@ -1,5 +1,6 @@
 package com.cooksys.groupfinal.mappers;
 
+import com.cooksys.groupfinal.dtos.NewTeamRequestDto;
 import com.cooksys.groupfinal.dtos.TeamDto;
 import com.cooksys.groupfinal.dtos.TeamRequestDto;
 import com.cooksys.groupfinal.entities.Team;
@@ -15,5 +16,9 @@ public interface TeamMapper {
   Set<TeamDto> entitiesToDtos(Set<Team> teams);
 
   Team requestToEntityDto(TeamRequestDto teamRequestDto);
+
+  Team dtoToEntity(TeamDto teamDto);
+
+  Team dtoToEntity(NewTeamRequestDto newTeam);
 
 }
